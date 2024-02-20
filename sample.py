f04dd0d75ebad4b4e23e1f3f2d7d503443d38dc6
xo6 <xo6@ccfep2.center.ims.ac.jp>
2024-02-21 08:18:46 +0900

    commit

---
+++ sample.py
@@ -0,0 +1,22 @@
+from ase.io import write, read
+from ase.calculators.emt import EMT
+from dmf import DirectMaxFlux
+
+ref_images = read('structures/idpp_emt.traj',index=':')
+
+mxflx = DirectMaxFlux(ref_images,nmove=3,
+    adaptive_method='full_tmax',)
+
+#set calcs
+for i,image in enumerate(mxflx.images):
+    image.calc = EMT()
+
+#create and solve the variational problem
+mxflx.create_problem()
+mxflx.problem.add_option('output_file','sample_ipopt.out')
+mxflx.solve_problem()
+
+#final path
+write('sample_fin.traj',mxflx.images)
+#history of x(tmax)
+write('sample_tmax.traj',mxflx.problem.history.images_tmax)
