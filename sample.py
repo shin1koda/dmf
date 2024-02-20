54230c0bb18f0dd729c523b26a60cdac8419e9da
xo6 <xo6@ccfep2.center.ims.ac.jp>
2024-02-21 08:45:01 +0900

    commit

--- sample.py
+++ sample.py
@@ -2,21 +2,23 @@ from ase.io import write, read
 from ase.calculators.emt import EMT
 from dmf import DirectMaxFlux
 
+# read idpp-interpolated images created from react.xyz and prod.xyz
+# see ASE's document for how to generate it
 ref_images = read('idpp_images.traj',index=':')
 
-mxflx = DirectMaxFlux(ref_images,nmove=3,
-    adaptive_method='full_tmax',)
+# set up direct MaxFlux object
+mxflx = DirectMaxFlux(ref_images,nmove=3,update_teval=True)
 
-#set calcs
+# set up calculators
 for i,image in enumerate(mxflx.images):
     image.calc = EMT()
 
-#create and solve the variational problem
+# create and solve the variational problem
 mxflx.create_problem()
 mxflx.problem.add_option('output_file','sample_ipopt.out')
 mxflx.solve_problem()
 
-#final path
+# write final path
 write('sample_fin.traj',mxflx.images)
-#history of x(tmax)
+# write history of x(tmax)
 write('sample_tmax.traj',mxflx.problem.history.images_tmax)
